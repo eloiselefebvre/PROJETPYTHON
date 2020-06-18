04474a08c21ac30f02b77799924ec0d0c0345e03
eloiselefebvre <62387368+eloiselefebvre@users.noreply.github.com>
2020-06-18 15:30:32 +0200

    Add files via upload

--- Principal.py
+++ Principal.py
@@ -5,28 +5,9 @@ from matplotlib import pyplot
 
 
 
-#>>
-#CREATION D'UNE FIGURE
-figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
 
-
-
-#>>
-#TRACAGE DE LA FIGURE 3D
-your_mesh = mesh.Mesh.from_file('/Users/eloiselefebvre/Desktop/Projet 2e année/V_HULL_Normals_Outward.STL')
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-scale = your_mesh.points.flatten(-1)
-axes.auto_scale_xyz(scale, scale, scale)
-
-#>>>>>>>>>pyplot.show()
-
-file = open('/Users/eloiselefebvre/Desktop/Projet 2e année/V_HULL_Normals_Outward.STL','r')
-
-
-#>>
 #OUVERTURE ET LECTURE DU FICHIER STL
+file = open('/Users/eloiselefebvre/Desktop/Projet 2e année/V_HULL_Normals_Outward.STL','r')
 file.readline()
 
 
@@ -47,6 +28,7 @@ Liste_Force_FaXn = []
 Liste_Force_FaYn = []
 Liste_Force_FaZn = []
 Liste_Fa = []
+Liste_Zg=[]
 gravity = 9.81
 MasseObjet = 10
 volume = 23
@@ -55,36 +37,114 @@ Epsilon = 0.1
 NbFacettes = 0
 counter = 0
 
-#>>
-#CREATION D'UNE LISTE CONTENANT CHAQUE LIGNE DU FICHIER STL
 
-#print('ok')
-for line in file :
-#print(counter,line)
-    counter += 1
-    ListeFacette.append(line.strip())
-
-#print(ListeFacette)
-
-file.close()
-
-
-
-#>>
-#TRI DES LIGNES OU TEXTES AFIN DE RECUPERER QUE CELLES DONT ON A BESOIN
-counter = 0
-N = 0
-A = 1
-B = 2
-C = 3
 #print(len(ListeFacette))
 
+def lecturefichier(Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,file) :
+    ListeFacette = []
+    #>>
+    #CREATION D'UNE LISTE CONTENANT CHAQUE LIGNE DU FICHIER STL
+    for line in file :
+        ListeFacette.append(line.strip())
+    #print(ListeFacette)
+    file.close()
+
+    A = 1
+    B = 2
+    C = 3
+    N = 0
+    counter = 0
+    NbFacettes = 0
+    while counter <= (len(ListeFacette)-7):
+        NbFacettes += 1
+        #print("Je rentre dans la boucle :")
+
+#>>
+        New_Liste.append(ListeFacette[counter])
+        #print(counter)
+        #print(ListeFacette[counter])
+        ligne = New_Liste[N].split(" ")
+        #print(ligne)
+        #print('  ')
+        #print('>> Nouvelle Facette : ')
+        #print(' Normale : ')
+        Xn = float(ligne[2])
+        #print("Xn =", Xn)
+        Yn = float(ligne[3])
+        #print("Yn =", Yn)
+        Zn = float(ligne[4])
+        #print("Zn =", Zn)
+        N += 4
+        #print("  ")
+
+        Normale = np.array([Xn,Yn,Zn])
+        #print(Normale)
+        Mat_Normales = np.vstack((Mat_Normales,Normale))
+
+
+    #>>
+        New_Liste.append(ListeFacette[counter+2])
+        #print(ListeFacette[counter+2])
+        ligne_a = New_Liste[A].split(" ")
+        Xa = float(ligne_a[1])
+        Ya = float(ligne_a[2])
+        Za = float(ligne_a[3])
+        #print("Xa =",Xa)
+        #print("Ya =",Ya)
+        #print("Za =",Za)
+        A += 4
+        #print("  ")
+        pointA = np.array([Xa,Ya,Za])
+        Mat_PointA = np.vstack((Mat_PointA,pointA))
+
+
+    #>>
+        New_Liste.append(ListeFacette[counter+3])
+        #print(ListeFacette[counter+3])
+        ligne_b = New_Liste[B].split(" ")
+        Xb = float(ligne_b[1])
+        Yb = float(ligne_b[2])
+        Zb = float(ligne_b[3])
+        #print("Xb =",Xb)
+        #print("Yb =",Yb)
+        #print("Zb =",Zb)
+        B += 4
+        #print("  ")
+        pointB = np.array([Xb,Yb,Zb])
+        Mat_PointB = np.vstack((Mat_PointB,pointB))
+
+
+
+    #>>
+        New_Liste.append(ListeFacette[counter+4])
+        #print(ListeFacette[counter+4])
+        ligne_c = New_Liste[C].split(" ")
+        #print(ligne_c)
+        Xc = float(ligne_c[1])
+        Yc = float(ligne_c[2])
+        Zc = float(ligne_c[3])
+        #print("Xc =",Xc)
+        #print("Yc =",Yc)
+        #print("Zc =",Zc)
+        C += 4
+        #print("  ")
+        pointC = np.array([Xc,Yc,Zc])
+        Mat_PointC = np.vstack((Mat_PointC,pointC))
+
+        counter += 7
+
+    print("Coordonnées des normales",Mat_Normales)
+    print("Coordonnées des points A",Mat_PointA)
+    print("Coordonnées des points B",Mat_PointB)
+    print("Coordonnées des points C",Mat_PointC)
+    return (Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,NbFacettes)
 
 
 
 #>>>>>>>>>>>>CALCUL DE LA SURFACE D'UNE FACETTE<<<<<<<<<<<<
 def surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface):
     i = 1
+    #print(NbFacettes)
     while i <= NbFacettes :
         #print('ok')
         A = float(Mat_PointB[i,0]) - float(Mat_PointA[i,0])
@@ -99,11 +159,12 @@ def surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface):
         i += 1
         Surface_Facette = (( (B*Z)-(C*Y) )**2 + ( (C*X)-(A*Z) )**2 + ( (A*Y)-(B*X))**2)**0.5
         Liste_Surface.append(Surface_Facette)
-        for elt in Liste_Surface :
-            elt+=elt
+        #for elt in Liste_Surface :
+        #    elt+=elt
         #print(elt)
-    return Liste_Surface
     print ("Surface =",Liste_Surface)
+    return Liste_Surface
+
 
 
 
@@ -111,6 +172,7 @@ def surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface):
 #>>>>>>>>>>CALCUL DE L'ALTITUDE D'UNE FACETTE<<<<<<<<<<<
 def altitude (Mat_PointA,Mat_PointB,Mat_PointC,Liste_Altitude) :
     i = 1
+    NbFacettes = len(Mat_PointA)
     while i <= NbFacettes :
         Za = float(Mat_PointA[i,2])
         Zb = float(Mat_PointB[i,2])
@@ -122,8 +184,10 @@ def altitude (Mat_PointA,Mat_PointB,Mat_PointC,Liste_Altitude) :
         #print(Altitude_Facette)
         Liste_Altitude.append(Altitude_Facette)
         i+=1
+        #print('ok')
+        print ("Altitude =", Liste_Altitude)
     return Liste_Altitude
-    #print ("Altitude =", Liste_Altitude)
+
 
 
 
@@ -131,15 +195,19 @@ def altitude (Mat_PointA,Mat_PointB,Mat_PointC,Liste_Altitude) :
 def Zmin(Liste_Altitude):
     i = 0
     Zmin = Liste_Altitude[0]
-    while i <= len(Liste_Altitude)-1 :
+    print(len(Liste_Altitude))
+    while i < len(Liste_Altitude):
+        print('ok')
         if Liste_Altitude[i] <= Zmin :
             Zmin = Liste_Altitude[i]
             i += 1
         else :
             Zmin = Zmin
             i += 1
+            print(Zmin)
     return Zmin
     #print("Le point le plus bas est à", Zmin)
+
 
 def Zmax(Liste_Altitude):
     i = 0
@@ -187,14 +255,13 @@ def calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,
     #print(len(Liste_N_Normale))
     #print(len(Liste_Altitude))
     #print(len(Liste_Surface))
+    print("liste Zg",Liste_Zg)
+    print("listeAltitude Archimede",Liste_Altitude)
     while i <= len(Liste_N_Normale)-3 and j <= len(Liste_Altitude)-1 and k <= len(Liste_Surface)-1 :
         #print('ok')
         Xn = Liste_N_Normale[i]
-        print(Xn)
         Yn = Liste_N_Normale[i+1]
-        print(Yn)
         Zn = Liste_N_Normale[i+2]
-        print(Zn)
         #print("norme de la normale",normale)
         altitude = Liste_Altitude[j]
         #print("altitude =",altitude)
@@ -211,11 +278,11 @@ def calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,
         Liste_Force_FaYn.append(sumFaYn)
         Liste_Force_FaZn.append(sumFaZn)
     sumFaXn = sumFaXn*Masse_Volumique*gravity
-    sumFaYn = sumFaXn*Masse_Volumique*gravity
-    sumFaZn = sumFaXn*Masse_Volumique*gravity
+    sumFaYn = sumFaYn*Masse_Volumique*gravity
+    sumFaZn = sumFaZn*Masse_Volumique*gravity
     NormeVectForceArchimede = (((sumFaXn)**2)+((sumFaYn)**2)+((sumFaZn)**2))**0.5
 
-
+    print(NormeVectForceArchimede)
     return NormeVectForceArchimede
     #print(Liste_Force_FaXn)
     #print(Liste_Force_FaYn)
@@ -237,61 +304,69 @@ def CalculPoids(MasseObjet,gravity) :
     return Poids_Objet
 
 #>>>>>>>>CALCUL DU MOUVEMENT DE TRANSLATION<<<<<<<<<<<
-def translation(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity,MasseObjet):
+def translation(Liste_Altitude,ecart):
 
-    i = 2
-    forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
-    poids = CalculPoids(MasseObjet,gravity)
-    print(poids, forceArchimede)
-    #print("Poids =",PoidsObjet)
-    #print("Fa =",ForceArchimede)
-    #print("Normale =",Liste_Coordonnees_Normales)
-    #print("normale=",Liste_N_Normale)
-    Liste_N_Normale[2] += 1
-    while i < len(Liste_N_Normale)-3 :
-             if poids > forceArchimede :
-             #print('ok')
-                Liste_N_Normale[i+3] -= 0.1
-                i += 3
-             #print("New =",Liste_Coordonnees_Normales)
-             elif poids < forceArchimede :
-                Liste_N_Normale[i+3] += 0.1
-                i += 3
-    #print("new =",Liste_N_Normale)
+    i = 0
+    while i <= len(Liste_Altitude)-1 :
 
-    return Liste_N_Normale
+               Liste_Altitude[i] += ecart
+               i += 1
+
+    print("new =",Liste_Altitude)
+    return Liste_Altitude
+
+
 
 def dichotomie(Zmax,Epsilon,forcePoids,forceArchimede,Liste_Altitude,MasseObjet,gravity):
     Zmax=Zmax(Liste_Altitude)
     Zb= 0
     Za = -Zmax
-    liste_Zg =[]
-    forceArchimede_init = 0
-    poids = CalculPoids(MasseObjet,gravity)
 
-    #forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
-    while abs(poids - forceArchimede_init)>Epsilon:
+    print(Za)
+    forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
+    print("force archi",forceArchimede)
+    poids = CalculPoids(MasseObjet,gravity)
+    counter = 0
+    ecart = 0
+
+
+    while abs(poids - forceArchimede)>Epsilon and counter < 10:
+        counter+=1
+        print(">>>>>>>>itération   ",counter)
         print('OK')
         Zg= (Zb + Za)/2
-        print(Zg)
-        liste_Zg.append(Zg)
-        if forcePoids>forceArchimede:
-            translation(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity,MasseObjet)
+        print("      Zg_dicho     ",Zg)
+
+        if (poids-forceArchimede)>Epsilon:
+            ecart= Za-Zg
+            print("ecart", ecart)
+            print("listeAltitude",Liste_Altitude)
+            translation(Liste_Altitude,ecart)
+            print("listeAltitude",Liste_Altitude)
             forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
-            print('force =', forceArchimede)
-            print(forceArchimede)
-            Zg= Za
-            print("if",Zg)
-        elif forcePoids<forceArchimede :
-            translation(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity,MasseObjet)
+            print("     force archimède      ", forceArchimede)
+            Za= Zg
+            print("     if    ",Zg)
+
+
+        elif (poids-forceArchimede )<Epsilon:
+            ecart= Zb-Zg
+            print("ecart", ecart)
+            print("listeAltitude",Liste_Altitude)
+            translation(Liste_Altitude,ecart)
+            print("listeAltitude",Liste_Altitude)
             forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
-            Zg =Zb
-            print("elif",Zg)
+            Zb =Zg
+            print("     elif       ",Zg)
+
         else :
             print("Le bateau est à l'équilibre")
-            print("else",Zg)
+            print("     else       ",Zg)
 
     return Zg
+
+
+
 
 
 
@@ -301,128 +376,68 @@ def dichotomie(Zmax,Epsilon,forcePoids,forceArchimede,Liste_Altitude,MasseObjet,
 #ON PARCOURS TOUTE LA LISTE EN DEPOSANT DANS UNE AUTRE LISTE SEULEMENT LES LIGNES QUI NOUS INTERESSENT
 #ENSUITE ON GARDE SEULEMENT LES VALEURS DES COORDONNEES (ON NE GARDE QUE LES CHIFFRES)
 
-while counter <= (len(ListeFacette)-7):
-    NbFacettes += 1
-        #print("Je rentre dans la boucle :")
+if __name__ == '__main__':
 
-#>>
-    New_Liste.append(ListeFacette[counter])
-    #print(counter)
-    #print(ListeFacette[counter])
-    ligne = New_Liste[N].split(" ")
-    #print(ligne)
-    #print('  ')
-    #print('>> Nouvelle Facette : ')
-    #print(' Normale : ')
-    Xn = float(ligne[2])
-    #print("Xn =", Xn)
-    Yn = float(ligne[3])
-    #print("Yn =", Yn)
-    Zn = float(ligne[4])
-    #print("Zn =", Zn)
-    N += 4
-        #print("  ")
-
-    #Normale = np.array([Xn,Yn,Zn])
-    #print(Normale)
-    #Mat_Normales = np.vstack((Mat_Normales,Normale))
-
-
-#>>
-    New_Liste.append(ListeFacette[counter+2])
-    #print(ListeFacette[counter+2])
-    ligne_a = New_Liste[A].split(" ")
-    Xa = float(ligne_a[1])
-    Ya = float(ligne_a[2])
-    Za = float(ligne_a[3])
-    #print("Xa =",Xa)
-    #print("Ya =",Ya)
-    #print("Za =",Za)
-    A += 4
-    #print("  ")
-    pointA = np.array([Xa,Ya,Za])
-    Mat_PointA = np.vstack((Mat_PointA,pointA))
-
-
-#>>
-    New_Liste.append(ListeFacette[counter+3])
-    #print(ListeFacette[counter+3])
-    ligne_b = New_Liste[B].split(" ")
-    Xb = float(ligne_b[1])
-    Yb = float(ligne_b[2])
-    Zb = float(ligne_b[3])
-    #print("Xb =",Xb)
-    #print("Yb =",Yb)
-    #print("Zb =",Zb)
-    B += 4
-    #print("  ")
-    pointB = np.array([Xb,Yb,Zb])
-    Mat_PointB = np.vstack((Mat_PointB,pointB))
+    #>>
+    #CREATION D'UNE FIGURE
+    #figure = pyplot.figure()
+    #axes = mplot3d.Axes3D(figure)
 
 
 
-#>>
-    New_Liste.append(ListeFacette[counter+4])
-    #print(ListeFacette[counter+4])
-    ligne_c = New_Liste[C].split(" ")
-    #print(ligne_c)
-    Xc = float(ligne_c[1])
-    Yc = float(ligne_c[2])
-    Zc = float(ligne_c[3])
-    #print("Xc =",Xc)
-    #print("Yc =",Yc)
-    #print("Zc =",Zc)
-    C += 4
-    #print("  ")
-    pointC = np.array([Xc,Yc,Zc])
-    Mat_PointC = np.vstack((Mat_PointC,pointC))
+    #>>
+    #TRACAGE DE LA FIGURE 3D
+    #your_mesh = mesh.Mesh.from_file('/Users/eloiselefebvre/Desktop/Projet 2e année/V_HULL_Normals_Outward.STL')
+    #axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 
-    counter += 7
+    #scale = your_mesh.points.flatten(-1)
+    #axes.auto_scale_xyz(scale, scale, scale)
 
-#print(NbFacettes)
-#print("Coordonnées des normales",Mat_Normales)
-#print("Coordonnées des points A",Mat_PointA)
-#print("Coordonnées des points B",Mat_PointB)
-#print("Coordonnées des points C",Mat_PointC)
+    #>>>>>>>>>pyplot.show()
+
+    print("Début de récupération des données >>>>>>")
+
+    #>>
+    #OUVERTURE ET LECTURE DU FICHIER STL
+    file = open('/Users/eloiselefebvre/Desktop/Projet 2e année/V_HULL_Normals_Outward.STL','r')
+    file.readline()
 
 
 
-#>>>>>>>>>>>>>>>>>>>>>>>PROGRAMME PRINCIPAL<<<<<<<<<<<<<<<<
+    Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,NbFacettes = lecturefichier(Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,file)
+
+
+    #>>
+    #CALCUL DE LA SURFACE
+    surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface)
+
+    #>>
+    #CALCUL DE L'ALTITUDE
+    altitude(Mat_PointA,Mat_PointB,Mat_PointC,Liste_Altitude)
+
+    #>>
+    #CALCUL DU POINT LE PLUS BAS
+    Zmin(Liste_Altitude)
+
+    #>>
+    #RECUPERATION DES COORDONNEES DE LA NORMALE
+    #vect_Normale = normale(Mat_Normales,NbFacettes)
+
+    #>>
+    #>>>>>>>>>>>CALCUL DE LA POUSSEE D'ARCHIMEDE POUR LES FACETTES IMMERGEES<<<<<<<<<<<<<
+    #calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,gravity,Masse_Volumique)
+
+
+    #translation(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity,MasseObjet)
+    #forcePoids = CalculPoids(MasseObjet,gravity)
+    #forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
+    #dichotomie(Zmax,Epsilon,forcePoids,forceArchimede,Liste_Altitude,MasseObjet,gravity)
+    #print("Le point le plus bas est à z =",Zmin(Liste_Altitude))
+    #print(Liste_Altitude)
+    #print(Liste_Fa)
 
 
 
 
-#>>
-#CALCUL DE LA SURFACE
-#surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface)
-
-#>>
-#CALCUL DE L'ALTITUDE
-#altitude(Mat_PointA,Mat_PointB,Mat_PointC,Liste_Altitude)
-
-#>>
-#CALCUL DU POINT LE PLUS BAS
-#Zmin(Liste_Altitude)
-
-#>>
-#RECUPERATION DES COORDONNEES DE LA NORMALE
-#vect_Normale = normale(Mat_Normales,NbFacettes)
-
-#>>
-#>>>>>>>>>>>CALCUL DE LA POUSSEE D'ARCHIMEDE POUR LES FACETTES IMMERGEES<<<<<<<<<<<<<
-#calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,gravity,Masse_Volumique)
-
-
-#translation(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity,MasseObjet)
-#forcePoids = CalculPoids(MasseObjet,gravity)
-#forceArchimede = calculFa(Liste_N_Normale,Liste_Force_FaXn,Liste_Force_FaYn,Liste_Force_FaZn,Masse_Volumique,gravity)
-#dichotomie(Zmax,Epsilon,forcePoids,forceArchimede,Liste_Altitude,MasseObjet,gravity)
-#print("Le point le plus bas est à z =",Zmin(Liste_Altitude))
-#print(Liste_Altitude)
-#print(Liste_Fa)
-
-
-
-
-#print (Mat_Normales)
-#print(New_Liste)
+    #print (Mat_Normales)
+    #print(New_Liste)
