4ed0aa83134c96011281f60e4fc00e4c2f211cc8
eloiselefebvre <62387368+eloiselefebvre@users.noreply.github.com>
2020-06-18 15:29:01 +0200

    Add files via upload

---
+++ Tests/Surface_Test.py
@@ -0,0 +1,35 @@
+import numpy as np
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+import Principal
+print("fin de l'import du principal")
+
+
+counter = 0
+ListeFacette = []
+New_Liste = []
+Mat_Normales = np.array([0,0,0])
+Mat_PointA = np.array([0,0,0])
+Mat_PointB = np.array([0,0,0])
+Mat_PointC = np.array([0,0,0])
+Liste_Surface = []
+
+
+print("Début de récupération des données >>>>>>")
+
+
+file = open('/Users/eloiselefebvre/Desktop/Projet 2e année/STL_Test.stl','r')
+file.readline()
+
+
+
+Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,NbFacettes = Principal.lecturefichier(Mat_PointA,Mat_PointB,Mat_PointC,Mat_Normales,file)
+
+
+print("test de surface : ")
+
+Principal.surface(Mat_PointA,Mat_PointB,Mat_PointC,NbFacettes,Liste_Surface)
+
+print("fin du test")
